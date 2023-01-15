ac7a029d07c006f734c38f98514a75a23b56068e
takaNS <redohagi127@gmail.com>
2023-01-15 19:51:29 +0900

    change get_log.py

--- get_log.py
+++ get_log.py
@@ -1,10 +1,12 @@
 #! /usr/bin/python3
 
+from ast import Try
 import sys
+import serial
 
 try:
-	SerialPort = sys.argv[1]
-except IndexError:
+	S = serial.Serial(sys.argv[1],115200)
+except (IndexError,serial.serialutil.SerialException):
 	print("Please Choose Serial Port")
 	print("List Serial Ports\r\n")
 	print("Serial Port : device")
@@ -15,11 +17,21 @@ except IndexError:
 
 import datetime as dt
 import numpy as np
-import serial
 import os
+import asyncio
+import time
 
-S = serial.Serial(SerialPort,115200)
-
+async def func():
+	i = 0
+	while True :
+		print("logging now...",['-','\\','|','/'][i % 4],'\r\nPress [Ctrl + c] to stop\r',end = "\033[1A",)
+		i += 1
+		try :
+			time.sleep(0.5)
+		except KeyboardInterrupt:
+			print('\n\n\nstop logging')
+			sys.exit(0)
+			
 while True:
 	# buf = [[0] * 5 for i in range(10000)]
 	buf = np.empty((10000,5),dtype=object)
@@ -28,16 +40,20 @@ while True:
 	dir = now.strftime('./DATA/%Y/%m-%d')
 	file = now.strftime(dir + '/%m-%d_%H-%M.log')
 
-	while (now + dt.timedelta(minutes=1)).minute != dt.datetime.now().minute:
-		buf[cnt][0] = dt.datetime.now().strftime('%F_%H:%M:%S.%f')
-		try:
-			buf[cnt][1:] = S.readline().decode().split()
-		except ValueError:
-			# print('ValueError')
-			continue
+	asyncio.get_event_loop().run_until_complete(func())
 
-		print(buf[cnt])
-		cnt = cnt + 1
+	while (now + dt.timedelta(minutes=1)).minute != dt.datetime.now().minute:
+		while True:
+			buf[cnt][0] = dt.datetime.now().strftime('%F_%H:%M:%S.%f')
+			try:
+				buf[cnt][1:] = S.readline().decode().split()
+			except ValueError:
+				# print('ValueError')
+				continue
+			
+			print(buf[cnt])
+			cnt = cnt + 1
 
 	os.makedirs(dir, exist_ok=True)
 	np.savetxt(file, buf[:cnt-1], fmt="%s")
+
